92886a2a712cc5d86c2f4613f538080a73a2ca0e
Eduardo Borges <82469192+Eduardo-SBorges@users.noreply.github.com>
2021-11-22 16:35:49 -0300

    build: v.0.0.1 - first history done

---
+++ precache-manifest.e4f9ab3f58db7502480693112ca39161.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e93048ca4e5bf27353850370224a0b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f3f6ba7c71ab981f602d",
+    "url": "./static/js/2.63af59bd.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "./static/js/2.63af59bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a03549aab2a785744b62",
+    "url": "./static/js/main.ad419196.chunk.js"
+  },
+  {
+    "revision": "e5053c0e92d48cfc951d",
+    "url": "./static/js/runtime-main.63bfa4f6.js"
+  },
+  {
+    "revision": "1e8accece2f1444e13a2c46b4209eabf",
+    "url": "./static/media/icon-password.1e8accec.svg"
+  },
+  {
+    "revision": "4e554303ced692d210a574302a5fc057",
+    "url": "./static/media/icon-user.4e554303.svg"
+  },
+  {
+    "revision": "1d5d1754aed5c69a88168a3c76763147",
+    "url": "./static/media/img1.1d5d1754.png"
+  },
+  {
+    "revision": "17c1bff1e6389c7b1b334aaf72ab2acf",
+    "url": "./static/media/whiteLogo.17c1bff1.png"
+  }
+]);
