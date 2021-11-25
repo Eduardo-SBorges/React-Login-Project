8da81640dc9e2f9854b3706e319c0dfc1c8cc238
Renan Acosta <renangomesacosta@gmail.com>
2021-11-25 16:35:34 -0300

    buid: v1.0.0

---
+++ precache-manifest.6eb475bcfa9bc4d182529ab3cba4b2f0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9820e8b06fb28dec43ee15969a86f666",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ee00c37e64abaacf2ddb",
+    "url": "./static/js/2.6b2dba7d.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "./static/js/2.6b2dba7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6389bd88c016dcbdc986",
+    "url": "./static/js/main.49bc3e29.chunk.js"
+  },
+  {
+    "revision": "e5053c0e92d48cfc951d",
+    "url": "./static/js/runtime-main.63bfa4f6.js"
+  },
+  {
+    "revision": "a5d7f35c6d29dfd252317d1683de7ba0",
+    "url": "./static/media/ballLogoCompasso.a5d7f35c.png"
+  },
+  {
+    "revision": "809018f16911ca4e999433ad9492896b",
+    "url": "./static/media/icon-password.809018f1.svg"
+  },
+  {
+    "revision": "f96099b216d813cf8b3b84ece796fad0",
+    "url": "./static/media/icon-user.f96099b2.svg"
+  },
+  {
+    "revision": "1d5d1754aed5c69a88168a3c76763147",
+    "url": "./static/media/img1.1d5d1754.png"
+  },
+  {
+    "revision": "17c1bff1e6389c7b1b334aaf72ab2acf",
+    "url": "./static/media/whiteLogo.17c1bff1.png"
+  }
+]);
