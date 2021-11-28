469466f112ee9cb1f8e69ab09073eaa13c258371
= <cassianoss@live.com>
2021-11-28 15:24:10 -0300

    story: Text added to storybook

---
+++ src/stories/Text.stories.js
@@ -0,0 +1,21 @@
+import Text from "../components/Text";
+import React from "react";
+
+export default {
+  title: "Text",
+
+  component: Text,
+};
+
+const Template = (args) => <Text {...args} />;
+
+export const MainText = Template.bind({});
+
+MainText.args = {
+  text: "Continuar",
+  color: "red",
+  size: "16px",
+  weight: "bold",
+  margin: "0px",
+};
+
