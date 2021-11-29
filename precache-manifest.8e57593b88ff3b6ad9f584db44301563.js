42337b2d59208ab826a3e2d11bb7a20060b847a7
Eduardo Borges <82469192+Eduardo-SBorges@users.noreply.github.com>
2021-11-29 13:28:51 -0300

    build: v1.0.0

---
+++ precache-manifest.8e57593b88ff3b6ad9f584db44301563.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ac6b43ed527a7b223f387b2e029c602",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aeab34ff9bc183a8e06b",
+    "url": "./static/js/2.c9b0c9a3.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "./static/js/2.c9b0c9a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87d21c6c975821cb51b8",
+    "url": "./static/js/main.457c5007.chunk.js"
+  },
+  {
+    "revision": "e5053c0e92d48cfc951d",
+    "url": "./static/js/runtime-main.63bfa4f6.js"
+  },
+  {
+    "revision": "a5d7f35c6d29dfd252317d1683de7ba0",
+    "url": "./static/media/ballLogoCompasso.a5d7f35c.png"
+  },
+  {
+    "revision": "1e8accece2f1444e13a2c46b4209eabf",
+    "url": "./static/media/icon-password.1e8accec.svg"
+  },
+  {
+    "revision": "4e554303ced692d210a574302a5fc057",
+    "url": "./static/media/icon-user.4e554303.svg"
+  },
+  {
+    "revision": "1d5d1754aed5c69a88168a3c76763147",
+    "url": "./static/media/img1.1d5d1754.png"
+  },
+  {
+    "revision": "17c1bff1e6389c7b1b334aaf72ab2acf",
+    "url": "./static/media/whiteLogo.17c1bff1.png"
+  }
+]);
